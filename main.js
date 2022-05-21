13d668d40b2ede59a591a95ec7dc10ebc2ab880d
Adrika-Singhal <77188343+Adrika-Singhal@users.noreply.github.com>
2022-05-21 13:18:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+video = ""; 
+status1 = "";
+
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+   video.hide();
+    video.size(480,380);
+}
+
+function draw(){
+image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input_box = document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded !");
+    status1 = true;
+
+}
